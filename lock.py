897250b42fd6ea3d77f071e0f6e2acbcab2a2c2e
james <james@deuterium.local>
2014-08-15 18:37:37 -0700

    Lookup now classy.

--- lock.py
+++ lock.py
@@ -2,13 +2,21 @@ import os
 import __main__ as main
 
 class Lock:
+	"""Lockfile
+Manages lock file to avoid running multiple instances of the script.
+
+Args:
+	lock (str, optional): Set the filename for the lock file. Defaults to the main filename.
+
 	"""
-	Managing lock file to avoid running multiple simmultaneous requests to nginx
-	"""
-	#lock=str(os.path.basename(__file__))+".lock"
-	lock=str(os.path.basename(main.__file__))+".lock"
+	def __init__(self, lock=str(os.path.basename(main.__file__))+".lock"):
+		self.lock=lock
 
 	def create(self):
+		"""Creates a lockfile with the name of the main python script. Checks for existing lock before creating.
+		Returns:
+			True if sucessful, Flase otherwise.
+		"""
 		if self.check():
 			with open(self.lock, 'w') as file:
 				file.write(str(os.getpid()))
@@ -17,6 +25,10 @@ class Lock:
 		else: return False
 
 	def check(self):
+		"""Checks if the lockfile exits
+		Returns:
+			True if process is unlocked (no lock, or stale lock), false otherwise.
+		"""
 		if os.path.isfile(self.lock):
 			with open(self.lock, 'r') as file:
 				pid=file.readline()
@@ -32,6 +44,10 @@ class Lock:
 		else:
 			return True 
 	def destroy(self):
+		"""Destroy lockfile
+		Returns: 
+			True if successful. Dies otherwise.
+		"""
 		try:
 			os.remove(self.lock)
 		except:
