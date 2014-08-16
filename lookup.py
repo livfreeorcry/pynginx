0b58bdd20d499639165166b8d7fc6aef0a05c2c4
james <james@deuterium.local>
2014-08-15 18:50:37 -0700

    Cleaned lookup.py
    removed extraneous definition.

--- lookup.py
+++ lookup.py
@@ -1,24 +1,6 @@
 import socket
 import json
 
-def lookup(address):
-	"""
-	reverse lookup to get hostname for nagios
-	needs IPv4 address (also happy with hostname...but then it just returns what you put in)
-	will accept IP with port (eg 10.10.10.10:1010)
-	returns hostname, strips rest of FQDN (eg returns 'song' from 'song.quotemedia.com')
-	"""
-	ip=address.split(':')[0]
-	try:
-		hostname=socket.gethostbyaddr(ip)[0].split('.')[0]
-	except socket.herror:
-		print "Unkown host"
-		return ip
-	except socket.gaierror as g:
-		print "Bad input. ", g
-	else:
-		return hostname
-
 class Lookup():
 	"""DNS lookup and (slow) cache
 	Reverse-lookup for for nginx/nagios cross talk.
@@ -32,16 +14,17 @@ class Lookup():
 		pass
 	
 	def lookup(self, address):
+		"""
+		"""
 		ip=self.ip(address)
 		try:
 			hostname=socket.gethostbyaddr(ip)[0].split('.')[0]
 		except socket.herror:
 			print "Unkown host"
 			return ip
-		except socket.gaierror as g:
-			print "Bad input. ", g
 		else:
 			return hostname
 
 	def ip(self, ip):
+
 		return ip.split(':')[0]
