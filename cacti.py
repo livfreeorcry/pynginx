35b7d392f97249c6b6e4a143d7581e845d2ac6b3
James Burke <james@armifi.com>
2014-10-03 13:18:40 -0700

    Fixed logging location since cacti breaks the path.

--- cacti.py
+++ cacti.py
@@ -1,24 +1,25 @@
 from sys import stdout
+logfile="/var/tmp/pynginx.log"
 def cacti(blob, names, subcommand):
 	if len(subcommand)<2: 
 		subcommand=['cacti','index']
 	if subcommand[1]=='index': 
 		for line in cactiIndex(blob): 
 			print line
-			with open("pynginx.log", "a") as log:
+			with open(logfile, "a") as log:
 				log.write(line+"\n")
 	elif subcommand[1]=='query': 
 		for line in cactiQuery(blob, subcommand[2], names): 
 			print line
-			with open("pynginx.log", "a") as log:
+			with open(logfile, "a") as log:
 				log.write(line+"\n")
 	elif subcommand[1]=='get':
 		stdout.write( str(cactiGet(blob, subcommand[2], subcommand[3])) )
-		with open("pynginx.log", "a") as log:
+		with open(logfile, "a") as log:
 				log.write(str(cactiGet(blob, subcommand[2], subcommand[3]))+"\n")
 	else: 
 		print "Unrecognized command: {0}".format(subcommand[1])
-		with open("pynginx.log", "a") as log:
+		with open(logfile, "a") as log:
 				log.write("Unrecognized command: {0}\n".format(subcommand[1]))
 
 def cactiIndex(blob):
