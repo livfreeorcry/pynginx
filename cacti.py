f42557e39a828a634bc77b0b0c7afca85687cbd0
James Burke <james@armifi.com>
2014-10-03 11:32:42 -0700

    Fixed some logging linebreak issues

--- cacti.py
+++ cacti.py
@@ -5,21 +5,21 @@ def cacti(blob, names, subcommand):
 	if subcommand[1]=='index': 
 		for line in cactiIndex(blob): 
 			print line
-			with open("pynginx.log") as log:
-				log.write(line)
+			with open("pynginx.log", "a") as log:
+				log.write(line+"\n")
 	elif subcommand[1]=='query': 
 		for line in cactiQuery(blob, subcommand[2], names): 
 			print line
-			with open("pynginx.log") as log:
-				log.write(line)
+			with open("pynginx.log", "a") as log:
+				log.write(line+"\n")
 	elif subcommand[1]=='get':
 		stdout.write( str(cactiGet(blob, subcommand[2], subcommand[3])) )
-		with open("pynginx.log") as log:
-				log.write(str(cactiGet(blob, subcommand[2], subcommand[3])))
+		with open("pynginx.log", "a") as log:
+				log.write(str(cactiGet(blob, subcommand[2], subcommand[3]))+"\n")
 	else: 
 		print "Unrecognized command: {0}".format(subcommand[1])
-		with open("pynginx.log") as log:
-				log.write("Unrecognized command: {0}".format(subcommand[1]))
+		with open("pynginx.log", "a") as log:
+				log.write("Unrecognized command: {0}\n".format(subcommand[1]))
 
 def cactiIndex(blob):
 	results=[]
