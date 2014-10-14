8dbdd4814067a61ee8958088f7bd7c3d486a1e04
James Burke <james@armifi.com>
2014-10-14 11:24:43 -0700

    Changed order of response message
    from eg "xml web1 down" to "web1 xml down"

--- nagios.py
+++ nagios.py
@@ -28,22 +28,22 @@ def nagiosCheckLB(blob, env=None):
                         elif (instance["state"] in warningStates) & (code != 2): 
                                 code=1
                                 results.append("  ".join([
-                                        upstream,
                                         lookup(instance["server"], env), #gets the hostname
+                                        upstream,
                                         instance["state"],
                                         ]))
                         elif instance["state"] in criticalStates: 
                                 code=2
                                 results.append("  ".join([
-                                        upstream,
                                         lookup(instance["server"], env), #gets the hostname
+                                        upstream,
                                         instance["state"],
                                         ]))
                         elif (code!=2) | (code!=1) : 
                                 code=3
                                 results.append("  ".join([
-                                        upstream,
                                         lookup(instance["server"], env), #gets the hostname
+                                        upstream,
                                         instance["state"],
                                         ]))                  
         return results, code
