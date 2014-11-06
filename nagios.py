459abd4a84122b962ef5da7bff5e2afa92e3f2ec
James Burke <james@armifi.com>
2014-11-06 13:41:57 -0800

    New command for nagios check: connections
    Well check active connections against max_conns. Set warn / critical load percentage thresholds separated by ':' after commnd. Looks like this:
    
    pynginx -a host nagios connections 50:90

--- nagios.py
+++ nagios.py
@@ -7,6 +7,11 @@ def nagios(blob, subcommand, regex, env=None):
 
 	if (len(subcommand)<2) or (subcommand[1]=='check') : #Deafult to this if too few arguments provided
                 response, exitCode = nagiosCheckLB(blob, env)
+                response = ", ".join(response)
+        elif (subcommand[1]=='connections') and (len(subcommand)>=3):
+                warn,crit = subcommand[2].split(':')
+                response, exitCode = nagiosMaxConns(blob, int(warn), int(crit))
+                response = ", ".join(response)
         else: 
                 response= "Unrecognized command: {0}".format(subcommand[1])
                 exitCode=3
@@ -14,8 +19,33 @@ def nagios(blob, subcommand, regex, env=None):
         elif exitCode==1: serviceState = "WARNING"
         elif exitCode==2: serviceState = "CRITICAL"
         else: serviceState="UNKNOWN"
-        print "{0} {1}".format(serviceState, ", ".join(response))
+        print "{0} {1}".format(serviceState, response)
         sys.exit(exitCode)
+
+def nagiosMaxConns(blob, warn, crit):
+        results=[]
+        code=0
+        for upstream in blob["upstreams"]:
+                for instance in blob["upstreams"][upstream]:
+                        try:
+                                pct = (int(instance["active"]) * 100) / int(instance["max_conns"])
+                                if pct > crit:
+                                        code = 2
+                                        results.append("  ".join([
+                                                lookup(instance["server"], env), #gets the hostname
+                                                upstream,
+                                                instance["active"],
+                                        ]))
+                                elif pct > warn and code != 2:
+                                        code = 1
+                                        results.append("  ".join([
+                                                lookup(instance["server"], env), #gets the hostname
+                                                upstream,
+                                                instance["active"],
+                                        ]))
+                        except:
+                                pass
+        return results, code
 
 def nagiosCheckLB(blob, env=None):
         results=[]
@@ -35,3 +65,4 @@ def nagiosCheckLB(blob, env=None):
                                 elif (instance["state"] in warningStates) and (code != 2): code = 1
                                 elif code == 0: code = 3
         return results, code
+ 
